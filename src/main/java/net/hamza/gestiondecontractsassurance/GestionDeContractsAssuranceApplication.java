ad664e5670bb0c49ffc1f54159c2ca00697de76e
elcadiHamza <email_du_compte_rick679>
2026-05-11 09:14:07 +0100

    creating the project

---
+++ src/main/java/net/hamza/gestiondecontractsassurance/GestionDeContractsAssuranceApplication.java
@@ -0,0 +1,13 @@
+package net.hamza.gestiondecontractsassurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeContractsAssuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeContractsAssuranceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/hamza/gestiondecontractsassurance/GestionDeContractsAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hamza.gestiondecontractsassurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeContractsAssuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
